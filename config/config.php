961dd63fbaeb819c1610ff6ed07f00183377a953
simorgan <54029242+simorgan@users.noreply.github.com>
2020-10-22 13:39:24 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,6 +4,6 @@ define('ACTION_URL','action.php');
 
 // Database login details
 define('DB_HOST', 'localhost');
-define('DB_USER', 'raspi');
+define('DB_USER', '*****');
 define('DB_PASS', '*******');
 define('DB_NAME', 'seniorMembers');
